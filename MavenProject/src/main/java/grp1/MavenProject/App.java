df947bd38892d4f1d098e0c628957ffa8d49682e
gladwin2802 <ajgladwin2802@gmail.com>
2024-05-26 22:56:31 +0530

    first commit through eclipse

---
+++ MavenProject/src/main/java/grp1/MavenProject/App.java
@@ -0,0 +1,13 @@
+package grp1.MavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Uploading maven project to github using eclipse" );
+    }
+}
